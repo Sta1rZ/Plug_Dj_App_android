401bc90218f3f27a983b75860f2f69f95ca27aeb
Sta1rZ <alan.longuet@supelec.fr>
2014-06-29 18:55:18 +0200

    Revert "Plug_Dj"
    This reverts commit f817283857a5a9241e17455e39ffc4eb716476a9.

---
+++ Plug_Dj_app/src/com/example/plug_dj_app/Welcome_activity.java
@@ -0,0 +1,15 @@
+package com.example.plug_dj_app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Welcome_activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_activity);
+    }
+}
