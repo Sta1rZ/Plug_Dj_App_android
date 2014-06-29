28e82ba4fabfd4ca3d4ead24e6dae2337ea21fbd
Sta1rZ <alan.longuet@supelec.fr>
2014-06-29 17:49:39 +0200

    Re_base_project

--- Plug_Dj_app/src/com/example/plug_dj_app/Welcome_activity.java
+++ Plug_Dj_app/src/com/example/plug_dj_app/Welcome_activity.java
@@ -1,15 +1,15 @@
-package plug.app_android;
+package com.example.plug_dj_app;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+public class Welcome_activity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_welcome_activity);
     }
 }
